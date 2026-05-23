b9a1c2e22618d0e07de56cfa0f4f6977b171d06c
tomoyuki84226 <tomo@zoyo.info>
2026-05-23 21:46:00 +0900

    動画を自動再生開始

--- src/PochiFM.js
+++ src/PochiFM.js
@@ -187,6 +187,7 @@ app.fileOpen = function(file, index, pushState = true) {
     } else {
         next.setAttribute('disabled', 'disabled');
     }
+    if (object.nodeName == 'VIDEO') object.play();
     document.body.style.overflow = 'hidden';
 }
 
@@ -205,6 +206,7 @@ app.createViewObject = function(file) {
         const video = document.createElement('video');
         video.setAttribute('src', `${setting.orgDir}/${file.dir}/${file.id}.${file.ext}`);
         video.controls = true; 
+        video.playsInline = true;
         return video;
     }
 
